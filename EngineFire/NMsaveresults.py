6d0b1e5a7e6985feb0674aad15e047a9f864ed99
Sebastian Blum <sebast.blum@gmail.com>
2020-06-22 08:54:16 +0200

    Create NMsaveresults.py

---
+++ EngineFire/NMsaveresults.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 11 14:00:02 2019
+
+@author: s.blum@campus.tu-berlin.de
+"""
+
+import actms
+import csv
+
+resultlist = []
+
+
+'''
+SAVE DATA TO A CSV FILE
+'''
+
+def appendresultlist():
+    global resultlist
+    resultlist = actms.getlist()
+
+def writeResultsCsv(path):
+    global resultlist
+    with open(path +'/Results/Results_Engine_Fire_normative.csv', "a", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(resultlist)
+
+def writeReHeaderCsv(path):
+    header = ['Participant','Time.alert', \
+              'L.run','M.run','R.run', \
+              'L.focus','M.focus','R.focus', \
+              'Result','Time.focus.result', \
+              'L.action','M.action','R.action', \
+              'L.duration','M.duration','R.duration', \
+              'state.anticipation','correct','value.thrustlever', \
+              'uppertime','ticktime','lowertime','Time.action.result']
+    with open(path +'/Results/Results_Engine_Fire_normative.csv', "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(header)
